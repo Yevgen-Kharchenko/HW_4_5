64feb02b12fa74b115d53368c63e6a682d7ec056
Yevgen Kharchenko <yevgen.kharchenko.jd@gmail.com>
2019-09-23 12:26:01 +0300

    Junit Test

---
+++ src/test/java/task2/ChangeSignTest.java
@@ -0,0 +1,31 @@
+package task2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChangeSignTest {
+
+    private ChangeSign instance;
+
+    @Before
+    public void setUp() throws Exception {
+        instance = new ChangeSign();
+    }
+
+    @Test
+    public void shoutReturnPositiveWithNegativeSign() {
+        int result = instance.changeSign(10);
+        assert result == -10;
+    }
+    @Test
+    public void shoutReturnNegativeWithPositiveSign() {
+        int result = instance.changeSign(-10);
+        assert result == 10;
+    }
+    @Test
+    public void shoutReturnZeroWithZero() {
+        int result = instance.changeSign(0;
+        assert result == 0;
+
+    }
+}
