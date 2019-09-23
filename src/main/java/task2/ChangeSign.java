a71851641dc0c49c62456b9bdb5ae2531af3490e
Yevgen Kharchenko <yevgen.kharchenko.jd@gmail.com>
2019-09-23 11:37:46 +0300

    modification

--- src/main/java/task2/ChangeSign.java
+++ src/main/java/task2/ChangeSign.java
@@ -2,7 +2,14 @@ package task2;
 
 public class ChangeSign {
     public static void main(String[] args) {
-        int num = -569;
-        System.out.println(~num +1);
+        ChangeSign cs = new ChangeSign();
+        int num = 456;
+        System.out.println(cs.changeSign(num));
+    }
+
+    public int changeSign(int value) {
+        value = ~value + 1;
+        return value;
     }
 }
+
